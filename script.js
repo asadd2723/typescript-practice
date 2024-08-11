87b25feabb0552b92fb7b7fa9931cc8bf95c979d
asadd2723 <asadu3040@gmail.com>
2024-08-11 14:36:31 +0500

    typescript

---
+++ script.js
@@ -0,0 +1,4 @@
+var a = "202020";
+var arr = ["339", 3994, 304];
+console.log(a, arr);
+var b = "asad";
---
+++ script.ts
@@ -0,0 +1,5 @@
+let a:string = "202020";
+let arr:[string, number,number]=["339",3994,304];
+console.log(a, arr);
+
+let b:string = "asad";
