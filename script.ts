e4a711a5c25ad97f42aa5df4cd3f148c95f55623
asadd2723 <asadu3040@gmail.com>
2024-08-11 14:46:06 +0500

    data type in  typescript and tuple use in array

--- script.ts
+++ script.ts
@@ -1,5 +1,23 @@
+// string
 let a:string = "202020";
-let arr:[string, number,number]=["339",3994,304];
+
+//boolean
+let bool: boolean = true;
+
+//number
+let num: number =  1000;
+
+//array 
+let arr:[string] = ["string"]
+let arr1: [number] =[20];
+
+//tuple jab multi data types ki value add krni ho array ma
+let tuple:[string, number,number]=["339",3994,304];
+
+//tupleWithRest ..... tb use krta hain jab koi same data type ki multi values hoon unko array ma add krna hoo .
+let tuplewithrest:[...string[]] = ["string", "asad"]
+
 console.log(a, arr);
+
 
 let b:string = "asad";
